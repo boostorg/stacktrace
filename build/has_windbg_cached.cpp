ca4464d23c06b5910f5185290c830cbaac907ef2
Antony Polukhin <antoshkka@gmail.com>
2017-05-03 23:47:12 +0300

    Enable usage of MSVC specific thread local storage attributes

--- build/has_windbg_cached.cpp
+++ build/has_windbg_cached.cpp
@@ -4,13 +4,18 @@
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
+#include <boost/config.hpp>
 #include <cstring>
 #include <windows.h>
 #include "dbgeng.h"
 
 int foo() {
+#ifndef BOOST_NO_CXX11_THREAD_LOCAL
     static thread_local int i = 0;
-    
+#else
+    static __declspec(thread) int i = 0;
+#endif
+
     return i;
 }
 
