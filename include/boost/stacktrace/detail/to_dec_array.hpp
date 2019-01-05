d708d17ecdae84344069afaf283ac3e5e54a5465
Antony Polukhin <antoshkka@gmail.com>
2019-01-05 13:48:53 +0300

    Suppress warning (fixes #69)

--- include/boost/stacktrace/detail/to_dec_array.hpp
+++ include/boost/stacktrace/detail/to_dec_array.hpp
@@ -1,4 +1,4 @@
-// Copyright Antony Polukhin, 2016-2018.
+// Copyright Antony Polukhin, 2016-2019.
 //
 // Distributed under the Boost Software License, Version 1.0. (See
 // accompanying file LICENSE_1_0.txt or copy at
@@ -31,7 +31,7 @@ inline boost::array<char, 40> to_dec_array(std::size_t value) BOOST_NOEXCEPT {
     }
 
     for (std::size_t i = 1; i <= digits; ++i) {
-        ret[digits - i] = '0' + (value % 10);
+        ret[digits - i] = static_cast<char>('0' + (value % 10));
         value /= 10;
     }
 
