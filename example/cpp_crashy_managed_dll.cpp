3da421a3ff1deb4fb86aa733de58552a9d3ef35e
Tarmo Pikaro <tapika@yahoo.com>
2020-04-26 10:52:48 +0300

    feature: add crashy managed dll

---
+++ example/cpp_crashy_managed_dll.cpp
@@ -0,0 +1,16 @@
+// Copyright Tarmo Pikaro, 2016-2020.
+//
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+public ref class TestCppCrash
+{
+public:
+    static System::String^ getCallStack()
+    {
+        return nullptr;
+    }
+
+};
+
