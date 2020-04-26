5901f61b472072de4d405b1a8e1b66c017da1db4
Tarmo Pikaro <tapika@yahoo.com>
2020-04-26 13:33:38 +0300

    fix year

--- example/cpp_crashy_managed_dll/cpp_crashy_managed_dll.cpp
+++ example/cpp_crashy_managed_dll/cpp_crashy_managed_dll.cpp
@@ -1,4 +1,4 @@
-// Copyright Tarmo Pikaro, 2016-2020.
+// Copyright Tarmo Pikaro, 2020.
 //
 // Distributed under the Boost Software License, Version 1.0. (See
 // accompanying file LICENSE_1_0.txt or copy at
