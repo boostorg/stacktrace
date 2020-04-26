4b528394ab2181b4cd74b35bdee75cffc3fa681f
Tarmo Pikaro <tapika@yahoo.com>
2020-04-26 23:20:49 +0300

    tune: change call sequence to see native call stack

--- example/cpp_crashy_managed_dll/cpp_crashy_managed_dll.cpp
+++ example/cpp_crashy_managed_dll/cpp_crashy_managed_dll.cpp
@@ -3,7 +3,15 @@
 // Distributed under the Boost Software License, Version 1.0. (See
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
+#pragma unmanaged
 #include <boost/stacktrace.hpp>
+
+std::string getCallStack()
+{
+    return to_string(boost::stacktrace::stacktrace());
+}
+
+#pragma managed
 
 using namespace System;
 
@@ -12,7 +20,7 @@ public ref class TestCppCrash
 public:
     static System::String^ getCallStack()
     {
-        return gcnew String(to_string(boost::stacktrace::stacktrace()).c_str());
+        return gcnew String(::getCallStack().c_str());
     }
 };
 
