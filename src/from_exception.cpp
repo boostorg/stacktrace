708c4eda1fd7e955d272b2a1a3519d8cddb2f656
Antony Polukhin <antoshkka@gmail.com>
2023-12-02 21:13:03 +0300

    Towards a safe solution

--- src/from_exception.cpp
+++ src/from_exception.cpp
@@ -11,6 +11,17 @@
 #include <cstddef>
 #include <dlfcn.h>
 
+#if defined(__x86_64__) || defined(_M_X64)
+#define BOOST_STACKTRACE_ALWAYS_STORE_IN_PADDING 1
+#else
+#define BOOST_STACKTRACE_ALWAYS_STORE_IN_PADDING 0
+#endif
+
+#if !BOOST_STACKTRACE_ALWAYS_STORE_IN_PADDING
+#include <mutex>
+#include <unordered_map>
+#endif
+
 namespace {
 
 constexpr std::size_t kStacktraceDumpSize = 4096;
@@ -60,9 +71,13 @@ struct decrement_on_destroy {
   ~decrement_on_destroy() { --to_decrement; }
 };
 
-}  // namespace
+#if !BOOST_STACKTRACE_ALWAYS_STORE_IN_PADDING
+// Inspired by the coursework by Andrei Nekrashevich in the `libsfe`
+std::mutex mutex;
+std::unordered_map<void*, const char*> exception_to_dump_mapping;
+#endif
 
-#ifndef BOOST_STACKTRACE_USE_GLOBAL_MAP
+}  // namespace
 
 namespace __cxxabiv1 {
 
@@ -70,19 +85,29 @@ namespace __cxxabiv1 {
 
 // libc++-runtime specific function
 extern "C" BOOST_NOINLINE BOOST_SYMBOL_VISIBLE __attribute__((weak))
-void __cxa_decrement_exception_refcount(void *thrown_object) throw();
+void __cxa_increment_exception_refcount(void *primary_exception) throw();
+
+static bool is_libcpp_runtime() noexcept {
+    return __cxa_increment_exception_refcount;
+}
+
+#else
+
+static bool is_libcpp_runtime() noexcept { return false; }
 
 #endif
 
 static const char*& reference_to_empty_padding(void* ptr) noexcept {
-#if defined(__GNUC__) && defined(__ELF__)
-  if (__cxa_decrement_exception_refcount) {
-      // libc++-runtime
-      BOOST_ASSERT_MSG(sizeof(void*) == 4,
-                       "32bit platforms are unsupported with libc++ runtime");
-      return exception_begin_llvm_ptr(ptr)->reserve;
+  if (is_libcpp_runtime()) {
+    // libc++-runtime
+    BOOST_ASSERT_MSG(
+      sizeof(void*) == 4,
+      "32bit platforms are unsupported with libc++ runtime padding reusage. "
+      "Please report this issue to the library maintainters."
+    );
+    return exception_begin_llvm_ptr(ptr)->reserve;
   }
-#endif
+
   return exception_begin_gcc_ptr(ptr)->reserve;
 }
 
@@ -110,106 +135,31 @@ void* __cxa_allocate_exception(size_t thrown_size) throw() {
   constexpr size_t kSkip = 1;
   boost::stacktrace::safe_dump_to(kSkip, dump_ptr, kStacktraceDumpSize);
 
-  BOOST_ASSERT_MSG(reference_to_empty_padding(ptr) == nullptr,
-                   "Not zeroed out, unsupported implementation");
-  reference_to_empty_padding(ptr) = dump_ptr;
-
-  return ptr;
-}
-
-}  // namespace __cxxabiv1
-
-namespace boost { namespace stacktrace { namespace impl {
-
-BOOST_SYMBOL_EXPORT const char* current_exception_stacktrace() noexcept {
-  void* const exc_raw_ptr = get_current_exception_raw_ptr();
-  if (!exc_raw_ptr) {
-    return nullptr;
-  }
-
-  return __cxxabiv1::reference_to_empty_padding(exc_raw_ptr);
-}
-
-BOOST_SYMBOL_EXPORT void assert_no_pending_traces() noexcept {}
-
-}}}  // namespace boost::stacktrace::impl
-
-#else
-
-// Inspired by the coursework by Andrei Nekrashevich in the `libsfe`
-
-#include <mutex>
-#include <unordered_map>
-
-namespace {
-
-std::mutex mutex;
-std::unordered_map<void*, const char*> exception_to_dump_mapping;
-
-}  // namespace
-
-namespace __cxxabiv1 {
-
-extern "C" BOOST_SYMBOL_EXPORT
-void* __cxa_allocate_exception(size_t thrown_size) throw() {
-  static const auto orig_allocate_exception = []() {
-    void* const ptr = ::dlsym(RTLD_NEXT, "__cxa_allocate_exception");
-    BOOST_ASSERT_MSG(ptr, "Failed to find '__cxa_allocate_exception'");
-    return reinterpret_cast<void*(*)(size_t)>(ptr);
-  }();
-
-#ifndef NDEBUG
-  static thread_local std::size_t in_allocate_exception = 0;
-  BOOST_ASSERT_MSG(in_allocate_exception < 10, "Suspicious recursion");
-  ++in_allocate_exception;
-  const decrement_on_destroy guard{in_allocate_exception};
-#endif
-
-  static constexpr std::size_t kAlign = alignof(std::max_align_t);
-  thrown_size = (thrown_size + kAlign - 1) & (~(kAlign - 1));
-
-  void* const ptr = orig_allocate_exception(thrown_size + kStacktraceDumpSize);
-  char* const dump_ptr = static_cast<char*>(ptr) + thrown_size;
-
-  constexpr size_t kSkip = 1;
-  boost::stacktrace::safe_dump_to(kSkip, dump_ptr, kStacktraceDumpSize);
-
-  {
+#if !BOOST_STACKTRACE_ALWAYS_STORE_IN_PADDING
+  if (is_libcpp_runtime()) {
     const std::lock_guard<std::mutex> guard{mutex};
     exception_to_dump_mapping[ptr] = dump_ptr;
+  } else
+#endif
+  {
+    BOOST_ASSERT_MSG(
+      reference_to_empty_padding(ptr) == nullptr,
+      "Not zeroed out, unsupported implementation"
+    );
+    reference_to_empty_padding(ptr) = dump_ptr;
   }
 
   return ptr;
 }
 
-// Not called in libc++ as the __cxa_decrement_exception_refcount has an inlined
-// call to __cxa_free_exception
-extern "C" BOOST_SYMBOL_EXPORT
-void __cxa_free_exception(void* thrown_object) throw() {
-  static const auto orig_free_exception = []() {
-    void* const ptr = ::dlsym(RTLD_NEXT, "__cxa_free_exception");
-    BOOST_ASSERT_MSG(ptr, "Failed to find '__cxa_free_exception'");
-    return reinterpret_cast<void(*)(void*)>(ptr);
-  }();
+#if !BOOST_STACKTRACE_ALWAYS_STORE_IN_PADDING
 
-#ifndef NDEBUG
-  static thread_local std::size_t in_free_exception = 0;
-  BOOST_ASSERT_MSG(in_free_exception < 10, "Suspicious recursion");
-  ++in_free_exception;
-  const decrement_on_destroy guard{in_free_exception};
-#endif
-
-  {
-    const std::lock_guard<std::mutex> guard{mutex};
-    exception_to_dump_mapping.erase(thrown_object);
-  }
-
-  orig_free_exception(thrown_object);
-}
-
-// libc++ specific function
+// __cxa_free_exception is not called in libc++ as the
+// __cxa_decrement_exception_refcount has an inlined call to
+// __cxa_free_exception. Overriding libc++ specific function
 extern "C" BOOST_SYMBOL_EXPORT
 void __cxa_decrement_exception_refcount(void *thrown_object) throw() {
+  BOOST_ASSERT(is_libcpp_runtime());
   if (!thrown_object) {
     return;
   }
@@ -224,7 +174,7 @@ void __cxa_decrement_exception_refcount(void *thrown_object) throw() {
 
   // The following line has a race and could give false positives and false
   // negatives. In first case we remove the trace earlier, in the second case
-  // we get a small memory leak.
+  // we get a memory leak.
   if (exception_header->referenceCount == 1) {
     const std::lock_guard<std::mutex> guard{mutex};
     exception_to_dump_mapping.erase(thrown_object);
@@ -232,6 +182,8 @@ void __cxa_decrement_exception_refcount(void *thrown_object) throw() {
 
   orig_decrement_refcount(thrown_object);
 }
+
+#endif
 
 }  // namespace __cxxabiv1
 
@@ -243,21 +195,30 @@ BOOST_SYMBOL_EXPORT const char* current_exception_stacktrace() noexcept {
     return nullptr;
   }
 
-  const std::lock_guard<std::mutex> guard{mutex};
-  const auto it = exception_to_dump_mapping.find(exc_raw_ptr);
-  if (it != exception_to_dump_mapping.end()) {
-    return it->second;
+#if !BOOST_STACKTRACE_ALWAYS_STORE_IN_PADDING
+  if (__cxxabiv1::is_libcpp_runtime()) {
+    const std::lock_guard<std::mutex> guard{mutex};
+    const auto it = exception_to_dump_mapping.find(exc_raw_ptr);
+    if (it != exception_to_dump_mapping.end()) {
+      return it->second;
+    } else {
+      return nullptr;
+    }
+  } else
+#endif
+  {
+    return __cxxabiv1::reference_to_empty_padding(exc_raw_ptr);
   }
-
-  return nullptr;
 }
 
 BOOST_SYMBOL_EXPORT void assert_no_pending_traces() noexcept {
-  const std::lock_guard<std::mutex> guard{mutex};
-  BOOST_ASSERT(exception_to_dump_mapping.empty());
+#if !BOOST_STACKTRACE_ALWAYS_STORE_IN_PADDING
+  if (__cxxabiv1::is_libcpp_runtime()) {
+    const std::lock_guard<std::mutex> guard{mutex};
+    BOOST_ASSERT(exception_to_dump_mapping.empty());
+  }
+#endif
 }
 
 }}}  // namespace boost::stacktrace::impl
-
-#endif
 
