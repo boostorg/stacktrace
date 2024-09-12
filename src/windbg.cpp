9028c8a4131287fd1e270e007f1626105aa2297b
Antony Polukhin <antoshkka@gmail.com>
2024-09-12 11:14:26 +0300

    Attempt to fix weird build issues with CYGWIN and MacOS (fixes #88)

--- src/windbg.cpp
+++ src/windbg.cpp
@@ -4,7 +4,7 @@
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
-#if defined(__CYGWIN__) && !defined(_GNU_SOURCE)
+#ifndef _GNU_SOURCE
 #   define _GNU_SOURCE
 #endif
 
