412167dff3edc6b6a0297f07198a72434f3473ac
Maciej Czarnecki <mcczarny@gmail.com>
2024-12-25 14:50:35 +0100

    fixup! Make base addr offset default and redo MSVC implementation.

--- include/boost/stacktrace/detail/addr_base_msvc.hpp
+++ include/boost/stacktrace/detail/addr_base_msvc.hpp
@@ -1,3 +1,5 @@
+// Copyright Antony Polukhin, 2016-2024.
+//
 // Distributed under the Boost Software License, Version 1.0. (See
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
