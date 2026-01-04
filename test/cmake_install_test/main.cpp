902537345a72cab9063e5eaa1d7f2e2df5cf1864
Antony Polukhin <antoshkka@gmail.com>
2026-01-04 11:21:08 +0300

    Update copyright years

--- test/cmake_install_test/main.cpp
+++ test/cmake_install_test/main.cpp
@@ -1,5 +1,6 @@
 // Copyright 2019 Peter Dimov
-// Copyright 2022-2025 Antony Polukhin
+// Copyright Antony Polukhin, 2025-2026
+//
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
