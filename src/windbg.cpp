e3dc26c574b7862caea9f686d723c1a77b8c355d
Fedor Osetrov <fiosetrova@gmail.com>
2026-03-15 10:08:45 +0000

    compile dump for Windows

--- src/windbg.cpp
+++ src/windbg.cpp
@@ -4,11 +4,11 @@
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
+#define BOOST_STACKTRACE_INTERNAL_BUILD_LIBS
+#define BOOST_STACKTRACE_LINK
+
 #ifndef _GNU_SOURCE
 #   define _GNU_SOURCE
 #endif
 
-#define BOOST_STACKTRACE_INTERNAL_BUILD_LIBS
-#define BOOST_STACKTRACE_LINK
 #include <boost/stacktrace/detail/frame_msvc.ipp>
-#include <boost/stacktrace/safe_dump_to.hpp>
