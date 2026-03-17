7f4940cfad2f6ac960cff4c2bff302c958fe7917
Fedor Osetrov <fiosetrova@gmail.com>
2026-03-17 19:02:06 +0000

    add copyrights

--- modules/samples/usage_sample.cpp
+++ modules/samples/usage_sample.cpp
@@ -1,3 +1,10 @@
+// Copyright Antony Polukhin, 2025-2026.
+// Copyright Fedor Osetrov, 2025-2026.
+//
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
 #include <iostream>
 #include <boost/config.hpp>
 
