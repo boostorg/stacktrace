9c8eab07d96e55d441421ada524533b85c614fa1
Fedor Osetrov <fiosetrova@gmail.com>
2026-04-11 13:43:42 +0000

    bettern config name

--- include/boost/stacktrace/stacktrace.hpp
+++ include/boost/stacktrace/stacktrace.hpp
@@ -7,7 +7,7 @@
 #ifndef BOOST_STACKTRACE_STACKTRACE_HPP
 #define BOOST_STACKTRACE_STACKTRACE_HPP
 
-#include <boost/stacktrace/detail/stacktrace_config.hpp>
+#include <boost/stacktrace/detail/backend_config.hpp>
 
 #if !defined(BOOST_USE_MODULES) || defined(BOOST_STACKTRACE_INTERFACE_UNIT)
 
