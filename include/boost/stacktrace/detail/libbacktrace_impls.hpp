6e8707fb71b496ab43028d2e8742b228cd2fbc70
prbegd <90972353+prbegd@users.noreply.github.com>
2026-09-07 18:37:48 +0800

    Fix fallback logic for name() in libbacktrace backend. Do not use backtrace_syminfo if filename or function name are available (#234)

--- include/boost/stacktrace/detail/libbacktrace_impls.hpp
+++ include/boost/stacktrace/detail/libbacktrace_impls.hpp
@@ -49,14 +49,17 @@ inline void libbacktrace_syminfo_callback(void *data, std::uintptr_t pc, const c
 
 inline int libbacktrace_full_callback(void *data, std::uintptr_t /*pc*/, const char *filename, int lineno, const char *function) {
     pc_data& d = *static_cast<pc_data*>(data);
+    int return_value = 0;
     if (d.filename && filename) {
         *d.filename = filename;
+        return_value = 1;
     }
     if (d.function && function) {
         *d.function = function;
+        return_value = 1;
     }
     d.line = static_cast<std::size_t>(lineno);
-    return 0;
+    return return_value;
 }
 
 inline void libbacktrace_error_callback(void* /*data*/, const char* /*msg*/, int /*errnum*/) noexcept {
